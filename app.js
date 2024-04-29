c59560c448702a1005b88330302bdc11f261bb7a
Waseem khalil <waseemkhalilworl@gmail.com>
2024-04-29 12:26:05 -0700

    add file

--- app.js
+++ app.js
@@ -1,97 +1,38 @@
-// let what =prompt("What is your Name?")
-// console.log(what)
-// let answer = alert("Welcom " +what)
+let input = document.getElementById("Date-input");
+let Year = document.getElementById("Year");
+let Month = document.getElementById("Month");
+let Days = document.getElementById("Days");
 
-// const ageCalculate = () => {
-//     const today = new Date();
-//     const inputDate = new Date(document.getElementById("date-input").value);
+function calnextBirthday() {
+  let today = new Date();
+  let birthdate = new Date(input.value);
+  let nextbirthday;
 
-//     const Birthdetails =  {
-//         date: inputDate.getDate(),
-//         month: inputDate.getMonth() + 1 ,
-//         year: inputDate.getFullYear(),
-//     };
+  if (
+    birthdate.getMonth() < today.getMonth() ||
+    (birthdate.getMonth() === today.getMonth() &&
+      birthdate.getDate() < today.getDate())
+  ) {
+    nextbirthday = new Date(
+      today.getFullYear() + 1,
+      birthdate.getMonth(),
+      birthdate.getDate()
+    );
+  } else {
+    nextbirthday = new Date(
+      today.getFullYear(),
+      birthdate.getMonth(),
+      birthdate.getDate()
+    );
+  }
 
-//     const currentYear = today.getFullYear();
-//     const currentmonth = today.getMonth() + 1;
-//     const currentDate = today.getDate();
+  let difference = nextbirthday.getTime() - today.getTime();
+  let leftDays = Math.ceil(difference / (1000 * 3600 * 24));
+  let leftHours = Math.ceil(difference / (1000 * 3600));
+  let leftmins = Math.ceil(difference / (1000 * 60));
 
-//     if(isFutureDate(Birthdetails,currentYear,currentmonth,currentDate)) {
-//         alert("NOt Born Yet");
-//         displayResult("-","-","-");
-//         return;
-//     };
-        
-//     const { year, month, days } = calculateAge(
-//         Birthdetails,
-//         currentYear,
-//         currentmonth,
-//         currentDate           
-//     );
-//     displayResult=(days,month,year);
-
-//     const isFutureDate = (
-//         Birthdetails,
-//         currentYear,
-//         currentmonth,
-//         currentDate
-//     )=>{
-//         return (
-//             Birthdetails.year > currentYear ||
-//             (Birthdetails.year === currentYear &&
-//              (Birthdetails.year > currentmonth ||
-//               (Birthdetails.year === currentmonth &&
-//                 Birthdetails.date > currentDate )))
-//         )
-//     };
-
-//     const calculateAge = (Birthdetails,currentYear,currentmonth,currentDate) => {
-//         let years = currentYear - Birthdetails.year;
-//         let month , days
-        
-//         if(currentmonth < Birthdetails.month){
-//             year--;
-//             month = 12 - (Birthdetails.month - currentmonth);
-//         } else{
-//                month = currentDate < Birthdetails.month; 
-//         }
-
-//         if(currentDate < Birthdetails.date){
-//             month--;
-//             const lastmonth = currentmonth === 1 ? 12 :
-//             currentmonth - 1;
-//             const daysInlastmonth = getDaysInMonth
-//             (lastmonth,currentYear);
-//             days = daysInlastmonth - (Birthdetails.date - currentDate);
-//         }
-//         else{
-//             days = currentDate - Birthdetails.date;
-//         }
-//         return{year,month,days};
-//     };
-//     const getDaysInMonth = (month,year) => {
-//         const isleapYear = year % 4 === 0 && (year % 100
-//         != 0 || year % 400 === 0);
-//         const getDaysInMonth =[
-//             31,
-//             isleapYear ? 29 : 28,
-//             31,
-//             30,
-//             31,
-//             30,
-//             31,
-//             30,
-//             30,
-//             31,
-//             30,
-//             31,
-//         ];
-//         return getDaysInMonth[month - 1]; 
-//     };
-//     const displayResult =(bdate, bmonth, byear) => {
-//         document.getElementById("years").textContent= byear;
-//         document.getElementById("months").textContent= bmonth;
-//         document.getElementById("days").textContent= bdate;
-//     };
-//     document.getElementById("cals-age-btn").
-// };
+  // console.log( leftDays);
+  Year.innerHTML = "Your next birthday is in " + leftDays + "Days";
+  Month.innerHTML = "And in " + leftHours + "Hours";
+  Days.innerHTML = "And in " + leftmins + "Minuts";
+};
